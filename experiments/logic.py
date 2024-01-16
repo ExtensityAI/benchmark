8f4c65aae091155418ff0b89db5482ee1f7ddbe2
leoentersthevoid <c.leov@icloud.com>
2024-01-17 01:02:51 +0200

    z3 solver experiment

--- experiments/logic.py
+++ experiments/logic.py
@@ -1,74 +1,58 @@
 # good reference: https://ericpony.github.io/z3py-tutorial/guide-examples.htm
 
-from symai import Symbol, Expression
-from symai.components import FileReader, Execute, Function
-from symai.extended import Conversation
+from pathlib import Path
 
+from symai import Expression, Symbol
+from symai.components import Execute, FileReader, Function
+from symai.extended import Conversation
+from symai.post_processors import CodeExtractPostProcessor, StripPostProcessor
 from z3 import Solver, sat
 
 
-def register_custom_engine():
-    from symai.functional import EngineRepository
-    from engine import LLaMACppClientEngine
+def test_solve_puzzle():
+    reader   = FileReader()
+    problem  = reader("experiments/einstein_puzzle.txt")
+    solution = reader("experiments/einstein_puzzle_solution.txt")
 
-    ENGINE = "zephyr"
-    HOST   = "http://localhost"
-    PORT   = 8081
-    assert ENGINE in ["zephyr", "mistral"]
+    query = "Bob has two sons, John and Jay. Jay has one brother and father. The father has two sons. Jay's brother has a brother and a father. Who is Jay's brother?"
+    task  = """
+    Using the provided solution to the Einstein puzzle as a reference, apply the Z3 solver to solve the puzzle.
+    Return executable code following the markdown convention that upon running is solving the puzzle and assigning the result to an `answer` variable.
+    """
 
-    engine = LLaMACppClientEngine(host=HOST, port=PORT)
-    EngineRepository.register('neurosymbolic', engine, allow_engine_override=True)
+    template = f"""
+    [Problem]
+    {problem}
 
+    [Solution]
+    {solution}
 
-# tokens: 149
-problem = Symbol("""
-The Englishman lives in the red house.
-The Swede keeps dogs.
-The Dane drinks tea.
-The green house is just to the left of the white one.
-The owner of the green house drinks coffee.
-The Pall Mall smoker keeps birds.
-The owner of the yellow house smokes Dunhills.
-The man in the center house drinks milk.
-The Norwegian lives in the first house.
-The Blend smoker has a neighbor who keeps cats.
-The man who smokes Blue Masters drinks bier.
-The man who keeps horses lives next to the Dunhill smoker.
-The German smokes Prince.
-The Norwegian lives next to the blue house.
-The Blend smoker has a neighbor who drinks water.
-The question to be answered is: Who keeps fish?
-""")
+    [Task]
+    {task}
 
-reader = FileReader()
-runner = Execute(enclosure=True)
+    [Puzzle]
+    {query}
+    """
 
-# tokens: 1383
-solution = reader("einstein_puzzle_solution.txt")
+    # Nicely deal with the markdown
+    pp = CodeExtractPostProcessor()
 
-# tokens: 38
-query = Symbol("Bob has two sons, John and Jay. Jay has one brother and father. The father has two sons. Jay's brother has a brother and a father. Who is Jay's brother.")
-task = "Using the provided solution to the Einstein puzzle as a reference, apply the Z3 solver to solve the puzzle. Provide executable code that upon running is solving the puzzle."
+    # Execute the code
+    cv = Conversation(auto_print=True)
+    res = cv(template)
 
-# total: 1571
-template = f"""
-# Problem
-{problem.value}
+    # Extract the code
+    try:
+        exec(
+            pp(res.value, None, tag="python"),
+            globals()
+        )
+        answer = Symbol(globals()['answer'])
+        return "John" in answer.value
 
-# Solution
-{solution.value}
-
-# Problem
-{query.value}
-
-# Task
-{task}
-"""
+    except Exception as e:
+        return False
 
 
-cv = Conversation()
-res = cv(template)
-is_mistake = res.query("Is there a mistake in this program? Write `No` if there are no mistakes, or the line where there is a mistake.", temperature=0)
-breakpoint()
-
-
+if __name__ == "__main__":
+    answer = test_solve_puzzle()
