e751a132ee3f15455550b6b7e0c3c36c0b45e77f
Xpitfire <dinu.marius-constantin@hotmail.com>
2024-01-16 09:13:46 +0100

    Inserted files into in-memory index.

--- populate_index.py
+++ populate_index.py
@@ -1,16 +1,24 @@
 from symai.shellsv import retrieval_augmented_indexing
 from symai.functional import EngineRepository
-from symai.backend.engines.index.engine_pinecone import IndexEngine
+from symai.backend.engines.index.engine_pinecone import PineconeIndexEngine
+from symai.backend.engines.index.engine_vectordb import VectorDBIndexEngine
 
 
 def run():
     # Register embeddings engine globally for all Symbols from plugin
     EngineRepository.register_from_plugin('embedding', plugin='ExtensityAI/embeddings', kwargs={'model': 'all-mpnet-base-v2'}, allow_engine_override=True)
-    EngineRepository.register('index', IndexEngine(index_name='dataindex',
-                                                   index_dims=768,
-                                                   index_top_k=5))
+    # EngineRepository.register('index', PineconeIndexEngine(index_name='dataindex',
+    #                                                        index_dims=768,
+    #                                                        index_top_k=3))
+    vectorDB = VectorDBIndexEngine(index_name='dataindex',
+                                   index_dims=768,
+                                   index_top_k=3)
+    EngineRepository.register('index', vectorDB)
     # insert into the index
     retrieval_augmented_indexing('!src/evals/snippets', index_name='dataindex')
+    # need to persist in-memory to disk
+    vectorDB.save()
+
 
 
 if __name__ == '__main__':
